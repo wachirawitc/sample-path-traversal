0b4a2f106e50253f86e2851195987534fa634ff6
wachirawit <wachirawit.chankaew@gmail.com>
2022-11-18 11:02:06 +0700

    Create project

---
+++ src/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DownloadFile1(string fileName)
+        {
+            var filePath = Path.Combine(webHostEnvironment.WebRootPath, fileName);
+            if (System.IO.File.Exists(filePath) == false)
+            {
+                return NotFound();
+            }
+
+            return PhysicalFile(filePath, "text/plain");
+        }
+
+        public IActionResult DownloadFile2(string fileName)
+        {
+            IFileProvider physicalFileProvider = new PhysicalFileProvider(webHostEnvironment.WebRootPath);
+            var file = physicalFileProvider.GetFileInfo(fileName);
+            if (file.Exists == false || string.IsNullOrWhiteSpace(file.PhysicalPath))
+            {
+                return NotFound();
+            }
+
+            return PhysicalFile(file.PhysicalPath, "text/plain");
+        }
+
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+        }
+    }
+}
